093513ba480e4438b62090837dfab67685baf7e5
Carlos Alcantara <alcantaracarlosmiguelr@gmail.com>
2020-05-23 17:44:24 +0800

    automated and added kitty

---
+++ copy_dotfiles.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/zsh
+
+link ~/.zshrc .zshrc
+link ~/.xinitrc .xinitrc 
+link ~/.Xresources .Xresources 
+link ~/.Xmodmap .Xmodmap 
+link ~/.zshrc .zshrc
+link ~/.config/ranger/rc.conf .config/ranger/rc.conf
+link ~/.config/kitty/kitty.conf .config/kitty/kitty.conf
